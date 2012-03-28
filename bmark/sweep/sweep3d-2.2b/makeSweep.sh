cd720de675422cd3b38c2c68fe6a8a3341d01765
Tapasya Patki <patki1@hyperion0.llnl.gov>
2012-03-27 20:23:36 -0700

    Changed configure to use intel compiler.

--- bmark/sweep/sweep3d-2.2b/makeSweep.sh
+++ bmark/sweep/sweep3d-2.2b/makeSweep.sh
@@ -2,5 +2,6 @@
 #setenv F77 mpif77
 #export $F77=mpif77
 #sh ./configure
-./configure F77="mpif77" LIBS="-L/usr/lib64/slurm" LDFLAGS="-lpmi"
+#./configure F77="mpif77" LIBS="-L/usr/lib64/slurm" LDFLAGS="-lpmi"
+./configure F77="mpif77" CC=icc --with-mpi
 make
