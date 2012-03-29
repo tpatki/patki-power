cfcd76b55955b0c8eb8eaa79a4c9a566b3230ecb
Tapasya Patki <patki1@hyperion0.llnl.gov>
2012-03-29 13:25:06 -0700

    Adding a correctness script testsweep.sh

---
+++ bmark/sweep/sweep3d-2.2b/scripts/testsweep.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#MSUB -l nodes=16
+#MSUB -q pbatch
+#MSUB -l walltime=00:05:00
+
+
+srun --nodes=16 --ntasks=256 -ppbatch -e sweep.err -o sweep.dat --cpu_bind=sockets sh ../sweepCL.sh
+
