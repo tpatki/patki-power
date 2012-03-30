dc563a331b7520769d0327314de7a0bbf5a3bfcf
Tapasya Patki <patki1@hyperion0.llnl.gov>
2012-03-30 16:16:49 -0700

    Another try.

--- bmark/sweep/sweep3d-2.2b/scripts/testsweep.sh
+++ bmark/sweep/sweep3d-2.2b/scripts/testsweep.sh
@@ -4,5 +4,6 @@
 #MSUB -l walltime=00:05:00
 
 
-srun --nodes=16 --ntasks=256 -ppbatch -e sweep.err -o sweep.dat --cpu_bind=sockets sh sweepCL.sh
+#srun --nodes=16 --ntasks=256 -ppbatch -e sweep.err -o sweep.dat --cpu_bind=sockets sh ../sweep
+sh runCL.sh 16 256
 
