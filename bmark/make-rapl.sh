e540ddbf5e54fd79c0048ee07bc3b28185c8fe08
Tapasya Patki <patki1@hyperion0.llnl.gov>
2012-04-04 19:56:07 -0700

    Fixed makefile errors, I hope

--- bmark/make-rapl.sh
+++ bmark/make-rapl.sh
@@ -9,21 +9,31 @@
 #source /usr/local/tools/dotkit/init.sh && use ic-12.1.273 && use mvapich2-intel-1.7 && which icc
 #
 
+
+
+
 cd ../rapl/libmsr/
+make clean
 make > makeMSR.out 2>&1
+git add makeMSR.out
 git commit -a -m "Just made msr common "
+
 cd ./mpi/
+make clean
 make  > makeRAPL.out 2>&1
+git add makeRAPL.out
 git commit -a -m "Just made Librapl"
 
-cd ../../../
+cd ../../../bmark
 
 mkdir ./cg/bin
 cd ./cg/
+make clean
 sh makeCG.sh > makeCG.out 2>&1
+git add makeCG.out
 git commit -a -m "Just made CG with RAPL"
 
-cd ../../../
+cd ../../
 
 #cd ./cg/scripts
 #msub cg-experiment.sh
