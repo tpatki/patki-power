b8568787f88d3293c4e4aba66ebe830aa2bf868e
Tapasya Patki <patki1@hyperion0.llnl.gov>
2012-04-10 07:05:53 -0700

    FT scripts

---
+++ bmark/NAS/FT/scripts/ftCL.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#papiex -f dir-papi -e PAPI_L3_TCA -e PAPI_L3_TCM ../../bin/cg.D.$1
+../../bin/ft.D.$1
+
+
---
+++ bmark/NAS/FT/scripts/runCL.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# $1 = number of nodes desired
+# $2 = number of CPU cores desired
+set -x
+
+export BLR_BENCHMARK=$(HOME)/local/src/power/bmark/NAS/bin/ft.D.$2
+
+name=`date +%Y_%m_%d_%H_%M_%S_%N`
+mkdir $name
+cd $name
+touch out.dat
+env > env
+hostname >> info
+echo $name >> info
+uname -a >> info
+#srun --nodes=1 --ntasks=1 -ppfio cat /proc/cpuinfo | grep MHz >> info
+#srun --nodes=1 --ntasks=1 --time=1 -ppbatch cat /proc/cpuinfo | grep MHz >> info
+srun --nodes=1 --ntasks=1 -ppbatch cat /proc/cpuinfo | grep MHz >> info
+echo 'cores: '$2 >> info
+echo 'nodes: '$1 >> info
+
+#srun --nodes=$2 --ntasks=$3 -ppfio --cpu_bind=sockets ./cgCL.sh $3 > out.dat
+#srun --nodes=$1 --ntasks=$2 --time=10 -ppbatch --cpu_bind=sockets ../cgCL.sh $2 > out.dat
+
+#Note: This would only work for now and the current experiment-- as we are going to use only half the number of 
+#cores on the node, which is 8 cores. 
+#TP: Mar 22, 2012
+srun --nodes=$1 --ntasks=$2 -ppbatch -e ft.err -o out.dat --sockets-per-node=1 ../ftCL.sh $2
+
