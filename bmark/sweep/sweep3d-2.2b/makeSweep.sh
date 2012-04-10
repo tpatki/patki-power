2ac4a1982df2beca2080e9f1c814eae172772ea4
Tapasya Patki <tpatki@email.arizona.edu>
2012-04-09 20:07:38 -0700

    Sweep linked with librapl

--- bmark/sweep/sweep3d-2.2b/makeSweep.sh
+++ bmark/sweep/sweep3d-2.2b/makeSweep.sh
@@ -3,5 +3,7 @@
 #export $F77=mpif77
 #sh ./configure
 #./configure F77="mpif77" LIBS="-L/usr/lib64/slurm" LDFLAGS="-lpmi"
-./configure F77="mpif77" CC=icc --with-mpi
+
+export LD_LIBRARY_PATH=/home/tpatki/tapasya/patki-power/rapl/libmsr/lib:$LD_LIBRARY_PATH
+./configure F77="mpif77" LDFLAGS="-L/home/tpatki/tapasya/patki-power/rapl/libmsr/lib" LIBS="-lrapl" --with-mpi
 make
