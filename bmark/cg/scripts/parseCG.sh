b27f056e13d6a923d8e13d6263d274c1440fac16
Tapasya Patki <tpatki@email.arizona.edu>
2012-04-09 19:01:42 -0700

    committing parse script

--- bmark/cg/scripts/parseCG.sh
+++ bmark/cg/scripts/parseCG.sh
@@ -5,9 +5,9 @@ find . -name 'out.dat*' -print | sort -n > tmp
 cat tmp | xargs grep seconds | cut -d "=" -f 2 | sed 's/[ \t]*//g' > .time
 
 find . -name 'info*' -print | sort -n > tmp2
-cat tmp2 | xargs grep cores | cut -d ":" -f 2 | sed 's/[ \t]*//g' > .cores
-cat tmp2 | xargs grep nodes | cut -d ":" -f 2 |sed 's/[ \t]*//g'  > .nodes
-cat tmp2 | xargs grep MHz | sed -n '1~16p' | cut -d ":" -f 2 > .freq
+cat tmp2 | xargs grep cores | cut -d ":" -f 3 | sed 's/[ \t]*//g' > .cores
+cat tmp2 | xargs grep nodes | cut -d ":" -f 3 |sed 's/[ \t]*//g'  > .nodes
+cat tmp2 | xargs grep MHz | sed -n '1~16p' | cut -d ":" -f 3 > .freq
 
 paste -d ' ' .freq .nodes .cores .time > .table
 sed '1 i freq nodes cores time' .table > table.out
