c9f5a42ff98076d31090a9d946269be97e324249
Barry Rountree <rountree4@llnl.gov>
2012-04-10 21:52:25 -0700

    Just made CG with RAPL

--- bmark/NAS/CG/npbparams.h
+++ bmark/NAS/CG/npbparams.h
@@ -24,17 +24,17 @@ c number of nodes for which this version is compiled
         parameter (compiletime='10 Apr 2012')
         character*3 npbversion
         parameter (npbversion='3.3')
-        character*44 cs1
-        parameter (cs1='/home/tpatki/tapasya/test-mvapich/bin/mpif77')
+        character*6 cs1
+        parameter (cs1='mpif77')
         character*9 cs2
         parameter (cs2='$(MPIF77)')
         character*46 cs3
-        parameter (cs3='-Xlinker -rpath=/home/tpatki/tapasya/patki-...')
-        character*43 cs4
-        parameter (cs4='-I/home/tpatki/tapasya/test-mvapich/include')
-        character*4 cs5
-        parameter (cs5='-O3 ')
-        character*3 cs6
-        parameter (cs6='-O3')
+        parameter (cs3='-Xlinker -rpath=$(HOME)/local/src/power/rap...')
+        character*6 cs4
+        parameter (cs4='(none)')
+        character*9 cs5
+        parameter (cs5='-O3 -fPIC')
+        character*9 cs6
+        parameter (cs6='-O3 -fPIC')
         character*6 cs7
         parameter (cs7='randi8')
