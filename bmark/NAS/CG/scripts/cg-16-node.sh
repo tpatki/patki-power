046a6f940e6be48ee562673f312ab470689df89f
Tapasya Patki <tpatki@email.arizona.edu>
2012-04-11 11:59:25 -0700

    You needed the bmark directory in the path

--- bmark/NAS/CG/scripts/cg-16-node.sh
+++ bmark/NAS/CG/scripts/cg-16-node.sh
@@ -22,5 +22,5 @@ export MSR_DRAM_POWER_LIMIT=0x0
 export MSR_RANK_MOD=16
 
 echo 'Resetting now...'
-srun --nodes=16 --ntasks=256 -o rapl-dummy.out -e rapl-dummy.err ./rapl-dummy/rapl-dummy
+srun --nodes=16 --ntasks=256 -o rapl-dummy.out -e rapl-dummy.err ./bmark/rapl-dummy/rapl-dummy
 
