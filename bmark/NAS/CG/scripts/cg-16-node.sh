079f9c73b4c60e8077c56ff1614583846458e8d6
Barry Rountree <rountree4@llnl.gov>
2012-04-11 11:55:18 -0700

    Make cg script more robust.

--- bmark/NAS/CG/scripts/cg-16-node.sh
+++ bmark/NAS/CG/scripts/cg-16-node.sh
@@ -5,18 +5,22 @@
 
 #Call the child script that does the runs
 
+export POWER_DIR=${HOME}/local/src/power
+
+cd ${POWER_DIR}/bmark/NAS/CG/scripts
 sh cg-rapl-16-node.sh
+cd ${POWER_DIR}
 
 
 #Reset by running a 256 task cg benchmark...
-export PKG_MINW_MINS=0x38198
-export PP0_MINW_MINS=0x38198
-export DRAM_MINW_MINS=0x38078
+export PKG_MINW_MINS=0x0
+export PP0_MINW_MINS=0x0
+export DRAM_MINW_MINS=0x0
 export MSR_PKG_POWER_LIMIT=0x0
 export MSR_PP0_POWER_LIMIT=0x0
 export MSR_DRAM_POWER_LIMIT=0x0
 export MSR_RANK_MOD=16
 
 echo 'Resetting now...'
-srun --nodes=16 --ntasks=256 -o rapl-dummy.out -e rapl-dummy.err ../../../rapl-dummy/rapl-dummy
+srun --nodes=16 --ntasks=256 -o rapl-dummy.out -e rapl-dummy.err ./rapl-dummy/rapl-dummy
 
