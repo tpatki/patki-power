54ac9a33395798d71bd0118b0a00304c59f1a264
tpatki <tpatki@email.arizona.edu>
2012-04-10 17:50:04 -0700

    Fixed the cg-rapl-16-node script...

--- bmark/NAS/CG/scripts/cg-rapl-16-node.sh
+++ bmark/NAS/CG/scripts/cg-rapl-16-node.sh
@@ -27,17 +27,23 @@ export MSR_RANK_MOD=16
 #LD_LIB_PATH should not be needed, ideally
 #export LD_LIBRARY_PATH=$(HOME)/local/src/power/rapl/libmsr/lib:LD_LIBRARY_PATH
 
- for pkg in 0x038208 0x038280 0x0382f8
+ for pkg in 0x38198 0x038208 0x038280 0x0382f8
        do
                export MSR_PKG_POWER_LIMIT=${pkg}
 #               for dram in 0x038078 0x0380a0 0x0380c8 0x0380f0
 #		do
                     export MSR_DRAM_POWER_LIMIT=${dram}
 		    #Run CG with 16 nodes, with 4,8,16 cores/node
-		    sh ./runCG.sh 1 1 2 2
+		    sh ./runCG.sh 16 16 4 16
 #		done
 	done
 
+#Reset by running a 256 task cg benchmark...
+export PKG_MINW_MINS=0x38198
+export PP0_MINW_MINS=0x38198
+export DRAM_MINW_MINS=0x38078
 export MSR_PKG_POWER_LIMIT=0x0
-export MSR_DRAM_POWER_LIMIT=0x0
 export MSR_PP0_POWER_LIMIT=0x0
+export MSR_DRAM_POWER_LIMIT=0x0
+
+sh ./runCG.sh 16 16 16 16
