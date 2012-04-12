2f557bb633cfbb7d2c2836ddbfd3369295ebb6ee
Tapasya Patki <tpatki@email.arizona.edu>
2012-04-12 16:32:59 -0700

    nodes=1

--- test-mkstemp/submit.sh
+++ test-mkstemp/submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#MSUB -l nodes=4
+#MSUB -l nodes=1
 #MSUB -q pbatch
 #MSUB -l walltime=00:05:00
 
-srun --nodes=4 --ntasks=4 -e test-mkstemp.err -o out.dat ./test-mkstemp
+srun --nodes=1 --ntasks=4 -e test-mkstemp.err -o out.dat ./test-mkstemp
 
 
