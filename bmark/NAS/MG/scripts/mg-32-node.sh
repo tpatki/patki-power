8a06f0c8a1189ecf568909ee419a2bd6ea016700
Tapasya Patki <tpatki@email.arizona.edu>
2012-04-13 16:03:13 -0700

    MG scripts

---
+++ bmark/NAS/MG/scripts/mg-32-node.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#MSUB -l nodes=32
+#MSUB -q pbatch
+#MSUB -l walltime=01:40:00
+
+#Call the child script that does the runs
+
+export POWER_DIR=${HOME}/local/src/power
+
+echo $POWER_DIR
+
+cd ${POWER_DIR}/bmark/NAS/MG/scripts
+sh mg-rapl-32-node.sh
+cd ${POWER_DIR}/dummy-tmp
+
+
+#Reset by running a 256 task cg benchmark...
+export PKG_MINW_MINS=0x0
+export PP0_MINW_MINS=0x0
+export DRAM_MINW_MINS=0x0
+export MSR_PKG_POWER_LIMIT=0x0
+export MSR_PP0_POWER_LIMIT=0x0
+export MSR_DRAM_POWER_LIMIT=0x0
+export MSR_RANK_MOD=16
+
+echo 'Resetting now...'
+srun --nodes=32 --ntasks=512 -o rapl-dummy.out -e rapl-dummy.err ../bmark/rapl-dummy/rapl-dummy
+cd ..
--- bmark/NAS/MG/scripts/mgCL.sh
+++ bmark/NAS/MG/scripts/mgCL.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#papiex -f dir-papi -e PAPI_L3_TCA -e PAPI_L3_TCM ../../bin/cg.D.$1
-../../bin/mg.D.$1
+../../../bin/mg.D.$1
 
 
