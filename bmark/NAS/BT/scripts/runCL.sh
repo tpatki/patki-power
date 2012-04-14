fc2bb6c247f07e9e7237b76193d289a0c59e145d
Tapasya Patki <tpatki@email.arizona.edu>
2012-04-14 13:26:57 -0700

    BT scripts

---
+++ bmark/NAS/BT/scripts/runCL.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# $1 = number of nodes desired
+# $2 = number of CPU cores desired
+set -x
+
+export BMARK_STRING=bt.D.$2
+
+
+#The following is to set the right number of nodes and tasks in BT and SP
+
+n=$1
+cores=$2
+
+# Need to run the closest square 
+#128 tasks, 16 nodes, 8 cores/node
+#Closest sqaure is 121, so at 8 cores/node, you need 8 (cores) * 15(nodes) + 1 (last node) 
+
+if [ $cores -eq 128 ] && [ $n -eq 16 ]
+then
+	cores=121
+	n=16
+fi
+
+#128 tasks, 32 nodes, 4 cores/node
+#Closest sqaure is 121, so at 4 cores/node, you need 4 (cores) * 30(nodes) + 1 (last node) = total 31 nodes
+
+if [ $cores -eq 128 ] && [ $n -eq 32 ]
+then
+	cores=121
+	n=31
+fi
+
+
+#512 tasks, 32 nodes, 16 cores/node
+#Closest square is 484, so at 16 cores/node, you need 16 * 30 + 4 * 1 = total 31 nodes
+
+if [ $cores -eq 512 ] && [ $n -eq 32 ]
+then
+        cores=484
+        n=31
+fi
+
+#512 tasks, 64 nodes, 8 cores/node
+#Closest square is 484, so at 8 cores/node, you need 8 * 60 + 4 * 1 = total 61 nodes
+
+if [ $cores -eq 512 ] && [ $n -eq 64 ]
+then
+        cores=484
+        n=61
+fi
+
+
+#512 tasks, 128 nodes, 4 cores/node
+#Closest square is 484, so at 4 cores/node, you need 4 * 121  = total 121 nodes
+
+if [ $cores -eq 512 ] && [ $n -eq 128 ]
+then
+        cores=484
+        n=121
+fi
+
+#2048 tasks, 128 nodes, 16 cores/node
+#Closest square is 2025, so at 16 cores/node, you need 16 *126 + 9 * 1 = total 127 nodes
+
+if [ $cores -eq 2048 ] && [ $n -eq 128 ]
+then
+        cores=2025
+        n=127
+fi
+
+
+name=`date +%Y_%m_%d_%H_%M_%S_%N`
+mkdir $name
+cd $name
+touch out.dat
+env > env
+hostname >> info
+echo $name >> info
+uname -a >> info
+srun --nodes=1 --ntasks=1 -ppbatch cat /proc/cpuinfo | grep MHz >> info
+echo 'cores: '$2 >> info
+echo 'nodes: '$1 >> info
+
+
+srun --nodes=$n --ntasks=$cores -ppbatch -e bt.err -o out.dat --cpu_bind=sockets ../btCL.sh $2
+
