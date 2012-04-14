fefe8361980215acf195bc3f76a9c3207cb2cac3
Tapasya Patki <tpatki@email.arizona.edu>
2012-04-13 17:17:24 -0700

    added the blr-bmark

--- bmark/rapl-dummy/submit-2.sh
+++ bmark/rapl-dummy/submit-2.sh
@@ -3,6 +3,8 @@
 #MSUB -q pbatch
 #MSUB -l walltime=00:05:00
 
+export BLR_BENCHMARK=$HOME/local/src/power/bmark/rapl-dummy/rapl-dummy
+
 srun --nodes=2 --ntasks=2 -e dummy.err -o out.dat ./rapl-dummy
 
 
