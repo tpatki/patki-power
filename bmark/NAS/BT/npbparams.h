67587814cf7e55cce24cb4f60e0cfff89a155469
Barry Rountree <rountree4@llnl.gov>
2012-04-15 12:40:42 -0700

    cleanup

---
+++ bmark/NAS/BT/npbparams.h
@@ -0,0 +1,35 @@
+c NPROCS = 2025 CLASS = D
+c  
+c  
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class of the NPB
+c  in this directory. Do not modify it by hand.
+c  
+        integer maxcells, problem_size, niter_default
+        parameter (maxcells=45, problem_size=408, niter_default=250)
+        double precision dt_default
+        parameter (dt_default = 0.00002d0)
+        integer wr_default
+        parameter (wr_default = 5)
+        integer iotype
+        parameter (iotype = 0)
+        logical  convertdouble
+        parameter (convertdouble = .false.)
+        character*11 compiletime
+        parameter (compiletime='14 Apr 2012')
+        character*3 npbversion
+        parameter (npbversion='3.3')
+        character*6 cs1
+        parameter (cs1='mpif77')
+        character*9 cs2
+        parameter (cs2='$(MPIF77)')
+        character*46 cs3
+        parameter (cs3='-Xlinker -rpath=$(HOME)/local/src/power/rap...')
+        character*6 cs4
+        parameter (cs4='(none)')
+        character*9 cs5
+        parameter (cs5='-O3 -fPIC')
+        character*30 cs6
+        parameter (cs6='-O3 -fPIC  -Wl,--build-id=none')
+        character*6 cs7
+        parameter (cs7='randi8')
---
+++ bmark/NAS/MG/npbparams.h
@@ -0,0 +1,37 @@
+c NPROCS = 2048 CLASS = D
+c  
+c  
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class of the NPB
+c  in this directory. Do not modify it by hand.
+c  
+        integer nprocs_compiled
+        parameter (nprocs_compiled = 2048)
+        integer nx_default, ny_default, nz_default
+        parameter (nx_default=1024, ny_default=1024, nz_default=1024)
+        integer nit_default, lm, lt_default
+        parameter (nit_default=50, lm = 7, lt_default=10)
+        integer debug_default
+        parameter (debug_default=0)
+        integer ndim1, ndim2, ndim3
+        parameter (ndim1 = 7, ndim2 = 6, ndim3 = 6)
+        logical  convertdouble
+        parameter (convertdouble = .false.)
+        character*11 compiletime
+        parameter (compiletime='14 Apr 2012')
+        character*3 npbversion
+        parameter (npbversion='3.3')
+        character*6 cs1
+        parameter (cs1='mpif77')
+        character*9 cs2
+        parameter (cs2='$(MPIF77)')
+        character*46 cs3
+        parameter (cs3='-Xlinker -rpath=$(HOME)/local/src/power/rap...')
+        character*6 cs4
+        parameter (cs4='(none)')
+        character*9 cs5
+        parameter (cs5='-O3 -fPIC')
+        character*30 cs6
+        parameter (cs6='-O3 -fPIC  -Wl,--build-id=none')
+        character*6 cs7
+        parameter (cs7='randi8')
