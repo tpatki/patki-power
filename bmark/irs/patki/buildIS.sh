1829f406c055ae6fe41ed621bc86e086910961e6
Tapasya Patki <tpatki@email.arizona.edu>
2012-04-14 22:14:11 -0700

    Removes the local version before building

--- bmark/irs/patki/buildIS.sh
+++ bmark/irs/patki/buildIS.sh
@@ -3,7 +3,11 @@
 #export IRS_DIR=/home/tpatki/tapasya/test-p/patki-power/bmark/irs
 export IRS_DIR=$HOME/local/src/power/bmark/irs
 
-../scripts/irs_build irs-build $IRS_DIR -force
+#Remove the local version if it exists
+
+rm -rf $IRS_DIR/patki/irs-build
+
+../scripts/irs_build irs-build $IRS_DIR 
 cd $IRS_DIR/patki/irs-build/build
 ../../../scripts/irs_config chaos_3_x86_64
 make depend
