d3dbcc8cca449d4f8411df4c9028a82167982941
Tapasya Patki <tpatki@email.arizona.edu>
2012-04-15 12:20:33 -0700

    IRS Test script

---
+++ bmark/irs-with-path/irs/patki/scripts/irs-4-node.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#MSUB -l nodes=4
+#MSUB -q pbatch
+#MSUB -l walltime=00:10:00
+
+#Call the child script that does the runs
+
+export POWER_DIR=${HOME}/local/src/power
+
+echo $POWER_DIR
+
+export BMARK_STRING=irs.64
+
+cd ${POWER_DIR}/bmark/NAS/EP/scripts
+sh irs-rapl-4-node.sh
+cd ${POWER_DIR}/dummy-tmp
+
+
+#Reset by running a 256 task cg benchmark...
+export PKG_MINW_MINS=0x0
+export PP0_MINW_MINS=0x0
+export DRAM_MINW_MINS=0x0
+export MSR_PKG_POWER_LIMIT=0x0
+export MSR_PP0_POWER_LIMIT=0x0
+export MSR_DRAM_POWER_LIMIT=0x0
+export MSR_RANK_MOD=16
+
+echo 'Resetting now...'
+srun --nodes=4 --ntasks=64 -o rapl-dummy.out -e rapl-dummy.err ../bmark/rapl-dummy/rapl-dummy
+cd ..
