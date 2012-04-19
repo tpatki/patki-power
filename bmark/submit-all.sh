37098907c37c9aad2ba8cd1890367e21fc6b6603
Barry Rountree <rountree4@llnl.gov>
2012-04-18 23:11:47 -0700

    submit-all script errors.

--- bmark/submit-all.sh
+++ bmark/submit-all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ./NAS/BT/scripts
+echo `pwd`
 
 msub bt-16-node.msub
 msub bt-32-node.msub
@@ -8,6 +9,7 @@ msub bt-64-node.msub
 msub bt-128-node.msub
 
 cd ../../CG/scripts
+echo `pwd`
 
 msub cg-16-node.msub
 msub cg-32-node.msub
@@ -15,6 +17,7 @@ msub cg-64-node.msub
 msub cg-128-node.msub
 
 cd ../../EP/scripts
+echo `pwd`
 
 msub ep-16-node.msub
 msub ep-32-node.msub
@@ -22,6 +25,7 @@ msub ep-64-node.msub
 msub ep-128-node.msub
 
 cd ../../FT/scripts
+echo `pwd`
 
 msub ft-16-node.msub
 msub ft-32-node.msub
@@ -29,6 +33,7 @@ msub ft-64-node.msub
 msub ft-128-node.msub
 
 cd ../../LU/scripts
+echo `pwd`
 
 msub lu-16-node.msub
 msub lu-32-node.msub
@@ -36,6 +41,7 @@ msub lu-64-node.msub
 msub lu-128-node.msub
 
 cd ../../MG/scripts
+echo `pwd`
 
 msub mg-16-node.msub
 msub mg-32-node.msub
@@ -43,6 +49,7 @@ msub mg-64-node.msub
 msub mg-128-node.msub
 
 cd ../../SP/scripts
+echo `pwd`
 
 msub sp-16-node.msub
 msub sp-32-node.msub
@@ -50,6 +57,7 @@ msub sp-64-node.msub
 msub sp-128-node.msub
 
 cd ../../../sweep/sweep3d-2.2b/scripts
+echo `pwd`
 
 msub sweep-16-node.msub
 msub sweep-32-node.msub
@@ -57,6 +65,7 @@ msub sweep-64-node.msub
 msub sweep-128-node.msub
 
 cd ../../../sphot_v1.0/scripts
+echo `pwd`
 
 msub sphot-16-node.msub
 msub sphot-32-node.msub
