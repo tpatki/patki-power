49836626b1e1f5d0c8f52b0c0f066683753c416b
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-04-19 23:03:08 -0700

    a plot

--- plot_scripts/testPlot.R
+++ plot_scripts/testPlot.R
@@ -10,3 +10,9 @@ a = read.table(args[1], header=T);
 # plot total energy vs node and task count
 
 pkgLimit = sort(unique(a$pkg.clamp.0), decreasing = T)[1]
+
+a$avgNodePower = rowSums(a[, c('avg.pkg.0','avg.pkg.1','avg.dram.0','avg.dram.1')])
+
+sel = which(a$pkg.clamp.0 == pkgLimit)
+
+plot(a$cores[sel], a$avgNodePower[sel])
