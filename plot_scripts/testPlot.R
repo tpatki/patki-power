995241d3dafc5958b29a3a4bd4041762af1b1c61
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-04-20 11:04:16 -0700

    update plot; I will summarize the values at each configuration, color-code them, and add a legend.

--- plot_scripts/testPlot.R
+++ plot_scripts/testPlot.R
@@ -12,7 +12,17 @@ a = read.table(args[1], header=T);
 pkgLimit = sort(unique(a$pkg.clamp.0), decreasing = T)[1]
 
 a$avgNodePower = rowSums(a[, c('avg.pkg.0','avg.pkg.1','avg.dram.0','avg.dram.1')])
+a$coresPerNode = a$cores / a$nodes
 
 sel = which(a$pkg.clamp.0 == pkgLimit)
+coresPerNode = unique(a[sel, 'coresPerNode'])
+sel1 = intersect(sel, which(a$coresPerNode == coresPerNode[1]))
 
-plot(a$cores[sel], a$avgNodePower[sel])
+totalEnergy = a$avgNodePower[sel]*a$nodes[sel]*a$avg.time[sel]
+plot(a$cores[sel], totalEnergy, 
+		    main='total energy vs nodes & cores',
+#		    sub=paste(coresPerNode[1], 'cores per node'),
+		    xlab='cores',
+		    ylab='log total energy (joules)',
+		    log='y',
+		    ylim=c(1,10*max(totalEnergy)))
