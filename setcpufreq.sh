9ef0ac3041a628a29b8e886166082e9baca19169
Barry Rountree <rountree4@llnl.gov>
2012-04-20 19:26:33 -0700

    Fixed to work on rzmerl.

--- setcpufreq.sh
+++ setcpufreq.sh
@@ -4,17 +4,19 @@
 #pdsh -w "hyperion-fio[1-64]" sh setCpuFreq.sh 2667000 8 
 #(need proper command on merl)
 
-#$1 : CPU freq
-#$2: Num cores on the node
+# $1: CPU freq
+# $2: First core
+# $3: Last core
 
 #2668000 2667000 2533000 2400000 2267000 2133000 
 #2000000 1867000 1733000 1600000 
 
 cd ..
 
-echo $1
+echo "setting frequency to ${1} KHz on cores ${2} through ${3}"
 
-for i in `seq 0 $2`; do echo userspace > /sys/devices/system/cpu/cpu${i}/cpufreq/scaling_governor; done
+# Already set to userspace and we don't have write perms.
+#for i in `seq 0 $2`; do echo userspace > /sys/devices/system/cpu/cpu${i}/cpufreq/scaling_governor; done
 
-for i in `seq 0 $2`; do echo $1 > /sys/devices/system/cpu/cpu${i}/cpufreq/scaling_setspeed; done
+for i in `seq $2 $3`; do echo $1 > /sys/devices/system/cpu/cpu${i}/cpufreq/scaling_setspeed; done
 
