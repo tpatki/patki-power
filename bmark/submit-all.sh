b707612ab4286c738d7ded32501eeb2f0584ef54
Tapasya Patki <tpatki@email.arizona.edu>
2012-04-29 19:29:19 -0700

    Will only run 128-node runs now

--- bmark/submit-all.sh
+++ bmark/submit-all.sh
@@ -5,15 +5,15 @@ echo `pwd`
 
 #msub bt-16-node.msub
 #msub bt-32-node.msub
-msub bt-64-node.msub
-msub bt-128-node.msub
+#msub bt-64-node.msub
+#msub bt-128-node.msub
 
 cd ../../CG/scripts
 echo `pwd`
 
 #msub cg-16-node.msub
 #msub cg-32-node.msub
-msub cg-64-node.msub
+#msub cg-64-node.msub
 msub cg-128-node.msub
 
 cd ../../EP/scripts
@@ -21,7 +21,7 @@ echo `pwd`
 
 #msub ep-16-node.msub
 #msub ep-32-node.msub
-msub ep-64-node.msub
+#msub ep-64-node.msub
 msub ep-128-node.msub
 
 cd ../../FT/scripts
@@ -29,7 +29,7 @@ echo `pwd`
 
 #msub ft-16-node.msub
 #msub ft-32-node.msub
-msub ft-64-node.msub
+#msub ft-64-node.msub
 msub ft-128-node.msub
 
 cd ../../LU/scripts
@@ -37,7 +37,7 @@ echo `pwd`
 
 #msub lu-16-node.msub
 #msub lu-32-node.msub
-msub lu-64-node.msub
+#msub lu-64-node.msub
 msub lu-128-node.msub
 
 cd ../../MG/scripts
@@ -45,7 +45,7 @@ echo `pwd`
 
 #msub mg-16-node.msub
 #msub mg-32-node.msub
-msub mg-64-node.msub
+#msub mg-64-node.msub
 msub mg-128-node.msub
 
 cd ../../SP/scripts
@@ -53,7 +53,7 @@ echo `pwd`
 
 #msub sp-16-node.msub
 #msub sp-32-node.msub
-msub sp-64-node.msub
+#msub sp-64-node.msub
 msub sp-128-node.msub
 
 cd ../../../sweep/sweep3d-2.2b/scripts
@@ -61,7 +61,7 @@ echo `pwd`
 
 #msub sweep-16-node.msub
 #msub sweep-32-node.msub
-msub sweep-64-node.msub
+#msub sweep-64-node.msub
 msub sweep-128-node.msub
 
 cd ../../../sphot_v1.0/scripts
@@ -69,6 +69,6 @@ echo `pwd`
 
 #msub sphot-16-node.msub
 #msub sphot-32-node.msub
-msub sphot-64-node.msub
+#msub sphot-64-node.msub
 msub sphot-128-node.msub
 
