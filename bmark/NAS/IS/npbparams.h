0756f593063db011a92d2c7063ee0109acb1f097
Barry <rountree4@llnl.gov>
2012-05-21 23:23:22 -0700

    cg 4-node run on cab

---
+++ bmark/NAS/IS/npbparams.h
@@ -0,0 +1,15 @@
+#define CLASS 'D'
+#define NUM_PROCS 2048
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   */
+   
+#define COMPILETIME "21 May 2012"
+#define NPBVERSION "3.3"
+#define MPICC "mpicc"
+#define CFLAGS "-O3 -fPIC"
+#define CLINK "$(MPICC)"
+#define CLINKFLAGS "-O3 -fPIC  -Wl,--build-id=none"
+#define CMPI_LIB "-Xlinker -rpath=$(HOME)/local/src/power/rap..."
+#define CMPI_INC "(none)"
