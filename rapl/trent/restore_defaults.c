8a5d714f1c56b90209d703bcae2d7fa1e993ae6b
Barry <rountree4@llnl.gov>
2012-05-25 12:15:45 -0700

    Simplified.

--- rapl/trent/restore_defaults.c
+++ rapl/trent/restore_defaults.c
@@ -1,13 +1,33 @@
+/* Run this in the SLURM epilog in order to reset MSRs to known good values.
+ *
+ * At the moment, this enables turbo and sets the package power limit to the
+ * default value provided by Appro.
+ *
+ * Assumes 2 packages per node and 8 cores per package.
+ *
+ * Note that changing turbo state requires read-modify-write sequence.
+ */
+
 #include <stdio.h>
 #include <errno.h>
 #include <fcntl.h>
-#include "restore_defaults.h"
+#include <stdint.h>
+#include <sys/types.h>  // off_t
+#include <unistd.h>
+
+#define NUM_PACKAGES 2
+#define NUM_CORES_PER_PACKAGE 8
+
+#define IA32_PERF_CTL		0x199 // setting bit 32 high DISABLES turbo mode.
+#define MSR_PKG_POWER_LIMIT	0x610 // Section 14.7.3 "Package RAPL Domain"
+#define MSR_PP0_POWER_LIMIT	0x638 // Section 14.7.4 "PP0/PP1 RAPL Domains"
+#define MSR_DRAM_POWER_LIMIT	0x618  
+
+#define APPRO_DEFAULT_PKG_POWER_LIMIT 0x6845000148398
 
 static int core_fd[NUM_PACKAGES][NUM_CORES_PER_PACKAGE];
 
-
-
-void
+static void
 init_msr(){
         int i,j;
         char filename[1025];
@@ -21,8 +41,11 @@ init_msr(){
                         snprintf(filename, 1024, "/dev/cpu/%d/msr", i*NUM_CORES_PER_PACKAGE+j);
                         core_fd[i][j] = open( filename, O_RDWR );
 	                       if(core_fd[i][j] == -1){
-                               snprintf(filename, 1024, "%s::%d  Error opening /dev/cpu/%d/msr\n", __FILE__, __LINE__, i*NUM_CORES_PER_PACKAGE+j);
-                                perror(filename);
+                               snprintf(	filename, 
+					       	1024, 
+					       	"%s::%d  Error opening /dev/cpu/%d/msr\n", __FILE__, __LINE__, 
+					       	i*NUM_CORES_PER_PACKAGE+j);
+                               perror(filename);
                         }
 
                 }
@@ -31,9 +54,7 @@ init_msr(){
         initialized = 1;
 }
 
-
-
-void
+static void
 finalize_msr(){
         int i, j, rc;
         char filename[1025];
@@ -42,7 +63,10 @@ finalize_msr(){
                         if(core_fd[i][j]){
                                 rc = close(core_fd[i][j]);
                                 if( rc != 0 ){
-                                    snprintf(filename, 1024, "%s::%d  Error closing file /dev/cpu/%d/msr\n",__FILE__, __LINE__, i*NUM_CORES_PER_PACKAGE+j);
+                                    snprintf(	filename, 
+						1024, 
+						"%s::%d  Error closing file /dev/cpu/%d/msr\n",__FILE__, __LINE__, 
+						i*NUM_CORES_PER_PACKAGE+j);
                                         perror(filename);
                                 }
                         }else{
@@ -52,44 +76,22 @@ finalize_msr(){
         }
 }
 
-
-void
-read_msr_single_core(int cpu, int core, off_t msr, uint64_t *val){        
-	int rc, core_fd_idx;
-	char error_msg[1025];        
-	core_fd_idx = cpu*NUM_CORES_PER_PACKAGE+core;
-	rc = pread( core_fd[cpu][core], (void*)val, (size_t)sizeof(uint64_t),msr );
-   	if( rc != sizeof(uint64_t) ){
-                snprintf( error_msg, 1024, "%s::%d  pread returned %d. core_fd[%d][%d]=%d, cpu=%d, core=%d cpu+core=%d msr=%ld (0x%lx).  errno=%d\n", __FILE__, __LINE__, rc, cpu, core,core_fd[cpu][core], cpu, core, core_fd_idx, msr, msr, errno );
-
-                perror(error_msg);
-        }
-}
-
-
-void
+static void
 write_msr_single_core(int cpu, int core, off_t msr, uint64_t val){
         int rc, core_fd_idx;
         char error_msg[1025];
         core_fd_idx = cpu*NUM_CORES_PER_PACKAGE+core;
         rc = pwrite(core_fd[cpu][core], &val, (size_t)sizeof(uint64_t), msr );
         if( rc != sizeof(uint64_t) ){
-                snprintf( error_msg, 1024, "%s::%d  pwrite returned %d.  core_fd[%d][%d]=%d, cpu=%d, core=%d cpu+core=%d msr=%ld (0x%lx).  errno=%d\n",
+                snprintf( 	error_msg, 
+				1024, 
+				"%s::%d  pwrite returned %d.  core_fd[%d][%d]=%d, cpu=%d, core=%d cpu+core=%d msr=%ld (0x%lx).  errno=%d\n",
                                 __FILE__, __LINE__, rc, cpu, core, core_fd[cpu][core], cpu, core, core_fd_idx, msr, msr, errno );
                 perror(error_msg);
         }
 }
 
-void
-read_msr_all_cores_v(int cpu, off_t msr, uint64_t *val){
-        int j;
-        for(j=0; j<NUM_CORES_PER_PACKAGE; j++){
-                read_msr_single_core(cpu, 0, msr, &val[j]);
-        }
-}
-
-
-void
+static void
 write_msr_all_cores_v(int cpu, off_t msr, uint64_t *val){        int j;
         for(j=0; j<NUM_CORES_PER_PACKAGE; j++){
                 write_msr_single_core( cpu, j, msr, val[j] );
@@ -98,14 +100,38 @@ write_msr_all_cores_v(int cpu, off_t msr, uint64_t *val){        int j;
 
 
 
-void
+static void
 write_msr(int cpu, off_t msr, uint64_t val){
         write_msr_single_core( cpu, 0, msr, val );
 }
 
 
+static void
+read_msr_single_core(int cpu, int core, off_t msr, uint64_t *val){        
+	int rc, core_fd_idx;
+	char error_msg[1025];        
+	core_fd_idx = cpu*NUM_CORES_PER_PACKAGE+core;
+	rc = pread( core_fd[cpu][core], (void*)val, (size_t)sizeof(uint64_t),msr );
+   	if( rc != sizeof(uint64_t) ){
+                snprintf( 	error_msg, 
+				1024, 
+				"%s::%d  pread returned %d. core_fd[%d][%d]=%d, cpu=%d, core=%d cpu+core=%d msr=%ld (0x%lx).  errno=%d\n", 
+				__FILE__, __LINE__, rc, cpu, core,core_fd[cpu][core], cpu, core, core_fd_idx, msr, msr, errno );
 
-void
+                perror(error_msg);
+        }
+}
+
+static void
+read_msr_all_cores_v(int cpu, off_t msr, uint64_t *val){
+        int j;
+        for(j=0; j<NUM_CORES_PER_PACKAGE; j++){
+                read_msr_single_core(cpu, 0, msr, &val[j]);
+        }
+}
+
+
+static void
 enable_turbo(int package){
         int j;
         uint64_t val[NUM_CORES_PER_PACKAGE];
@@ -119,8 +145,9 @@ enable_turbo(int package){
 
 }
 
-
-void
+/* At the moment, the default is to leave turbo enabled.  Use this
+ * instead if the default changes.  --blr.
+static void
 disable_turbo(int package){
         int j;
         uint64_t val[NUM_CORES_PER_PACKAGE];
@@ -134,40 +161,17 @@ disable_turbo(int package){
         write_msr_all_cores_v( package, IA32_PERF_CTL, &val[0] );
 
 }
-
-
-void 
-restore_defaults()
-{
-        int package;
-
-       // Reset all limits.
-        //printf("\n Resetting default values of MSRs");
-        for(package=0; package<NUM_PACKAGES; package++){
-             write_msr( package, MSR_PKG_POWER_LIMIT, 0x6845000148398 );
-                   /*
-                     write_msr( package, MSR_PP0_POWER_LIMIT, 0 );
-                     #ifdef ARCH_062D
-                         write_msr( package, MSR_DRAM_POWER_LIMIT, 0 );
-                     # endif
-            */ // These are currently locked out.
-
-            //Note: The current default for turbo is that turbo boost is enabled. 
-            //If this changes in the future, there is a disable_turbo() module that
-            //can be used instead of the enable_turbo module.
-
-             enable_turbo(package);
-        }
-	
-
-}
+*/
 
 
 int 
 main()
 {
 	init_msr();
-	restore_defaults();
+        for(package=0; package<NUM_PACKAGES; package++){
+             write_msr( package, MSR_PKG_POWER_LIMIT, APPRO_DEFAULT_PKG_POWER_LIMIT );
+             enable_turbo(package);
+        }
 	finalize_msr();
 	return 0;
 }
