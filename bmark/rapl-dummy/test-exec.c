c31aa7e4131cc0f11d43aa2e900bccd0de99e55d
Tapasya Patki <patki1@llnl.gov>
2012-06-01 15:19:00 -0700

    tested test-exec and added a fork() call

--- bmark/rapl-dummy/test-exec.c
+++ bmark/rapl-dummy/test-exec.c
@@ -1,22 +1,41 @@
+/*Tapasya Patki
+ * How to run sequential or openMP code
+ * or another binary with librapl*/
+
 #include <mpi.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
 int
 main( int argc, char **argv ){
  int return_val=0;
 
+
  char *const args[] = 
    {"/bin/ls", "-l", NULL};
  
- MPI_Init(&argc, &argv);
+  int child_status;
+  pid_t child, cpid;
+ 
+  MPI_Init(&argc, &argv);
 
- return_val=execvp("ls", args);
+  child = fork();
+  if(child == 0){ /*I'm in the child process*/
 
- if(return_val == -1){
-    printf("\nCould not execute binary");
- }  
+  	return_val=execvp("/bin/ls", args);
 
- MPI_Finalize();
-
- return 0;
+ 	if(return_val == -1){
+    		printf("\nCould not execute binary");
+	} 
+  }  
+  else{ 
+	/*I'm in the parent process */
+	/*Wait for th child to terminate*/
+	do {
+	  cpid = wait(&child_status);
+	} while(cpid != child);
+ 
+	MPI_Finalize();
+ 	return child_status;
+    }
 }
